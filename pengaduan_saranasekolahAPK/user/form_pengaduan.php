881cde08be433139784b0aa112f031165fb65cea
NoNameNoIdentity <153426436+hanzzy1243@users.noreply.github.com>
2026-04-08 09:19:26 +0700

    Mengubah code dan penambahan style

--- pengaduan_saranasekolahAPK/user/form_pengaduan.php
+++ pengaduan_saranasekolahAPK/user/form_pengaduan.php
@@ -1,10 +1,38 @@
-<form method="POST" action="simpan_pengaduan.php" enctype="multipart/form-data">
-    Nama: <input type="text" name="nama"><br>
-    Kelas: <input type="text" name="kelas"><br>
-    Lokasi: <input type="text" name="lokasi"><br>
-    Jenis: <input type="text" name="jenis"><br>
-    Deskripsi: <textarea name="deskripsi"></textarea><br>
-    Foto: <input type="file" name="foto"><br>
-    <button type="submit">Kirim</button>
-</form>
-<a href="dashboard.php"><-Kembali ke dashboard</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Pengaduan Sarana Sekolah</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="form-container">
+        <h2>Buat Pengaduan</h2>
+
+        <form method="POST" action="simpan_pengaduan.php" enctype="multipart/form-data">
+            <label>Nama</label>
+            <input type="text" name="nama">
+
+            <label>Kelas</label>
+            <input type="text" name="kelas">
+
+            <label>Lokasi</label>
+            <input type="text" name="lokasi">
+
+            <label>Jenis</label>
+            <input type="text" name="jenis">
+
+            <label>Deskripsi</label>
+            <textarea name="deskripsi"></textarea>
+
+            <label>Foto</label>
+            <input type="file" name="foto">
+
+            <button type="submit">Kirim</button>
+        </form>
+    </div>
+<a href="dashboard.php" class="back">← Kembali ke Dashboard</a>
+</body>
+</html>
+
